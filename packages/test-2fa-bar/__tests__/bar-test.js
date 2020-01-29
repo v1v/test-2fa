f7e351122962dcb876ec20ffa4d26c99b9a705f9
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-01-29 16:34:58 +0000

    feat(test-2fa-bar): add more tests

--- packages/test-2fa-bar/__tests__/bar-test.js
+++ packages/test-2fa-bar/__tests__/bar-test.js
@@ -9,3 +9,9 @@ describe('Some dummy test bar 14', () => {
     expect(true).toBe(true);
   });
 });
+
+describe('Some dummy test bar 15', () => {
+  it('should always pass bar', () => {
+    expect(true).toBe(true);
+  });
+});
