194e392a9453b05d8ef9941bd479a89556623cc8
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-01-29 13:58:08 +0000

    fixes: debug

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
-set -xe
 
 ### Potentially to be delegated to the CI context preparation
 echo "//registry.npmjs.org/:_authToken=\${NPM_TOKEN}" > .npmrc
 
 ### Potentially to be delegated to the CI context preparation
 TOTP_CODE=$(curl -s -X GET "${VAULT_URL}" -H "X-Vault-Token: ${VAULT_NPM_TOTP_TOKEN}" | jq --raw-output ".data.code")
+
+## This is possible as TOTP code is used once and only once
+set -xe
 lerna publish --otp="${TOTP_CODE}"
 npm run github-release
