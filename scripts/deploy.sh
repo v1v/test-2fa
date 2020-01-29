a27f564bb66d1c12ea4f00432a86fb209ffadf75
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-01-29 15:41:00 +0000

    fixes: detached is not supported for releases

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,8 +8,18 @@ TOTP_CODE=$(curl -s -X GET "${VAULT_URL}" -H "X-Vault-Token: ${VAULT_NPM_TOTP_TO
 
 ## Running in the CI therefore it's not required to prompt for the versions
 FLAG=''
-if [ -n "${JENKINS_URL}" ] ||  [ -n "${TRAVIS}" ] ; then
+if [ -n "${JENKINS_URL}" ] || [ -n "${TRAVIS}" ] ; then
     FLAG='--yes'
+fi
+
+## Ensure it's not Detached
+if [ -n "${TRAVIS}" ] ; then
+    git checkout "${TRAVIS_BRANCH}"
+    GIT_CURRENT_SHA=$(git rev-parse HEAD)
+    if [ "${GIT_CURRENT_SHA}" != "${TRAVIS_COMMIT}" ] ; then
+        echo 'Current version does not match with the one that triggered this build'
+        git reset --hard "${TRAVIS_COMMIT}"
+    fi
 fi
 
 set -xe
