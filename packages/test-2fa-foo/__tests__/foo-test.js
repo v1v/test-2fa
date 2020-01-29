9a317f6c978d64083e3440558295b22f9b976247
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-01-29 17:24:37 +0000

    feat(test-2fa-foo): add more tests

--- packages/test-2fa-foo/__tests__/foo-test.js
+++ packages/test-2fa-foo/__tests__/foo-test.js
@@ -27,3 +27,9 @@ describe('Some dummy test 13', () => {
     expect(true).toBe(true);
   });
 });
+
+describe('Some dummy test 13', () => {
+  it('should always pass', () => {
+    expect(true).toBe(true);
+  });
+});
