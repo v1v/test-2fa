00d6861678b18da88d7156035f6ce188741fbff1
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-01-29 13:46:09 +0000

    Use the script instead

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,9 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+### Potentially to be delegated to the CI context preparation
 echo "//registry.npmjs.org/:_authToken=\${NPM_TOKEN}" > .npmrc
-totp=$(curl -s -X GET $VAULT_URL -H "X-Vault-Token: ${VAULT_NPM_TOTP_TOKEN}" | jq --raw-output ".data.code")
-npm publish . --otp=$totp
+
+### Potentially to be delegated to the CI context preparation
+TOTP_CODE=$(curl -s -X GET "${VAULT_URL}" -H "X-Vault-Token: ${VAULT_NPM_TOTP_TOKEN}" | jq --raw-output ".data.code")
+lerna publish --otp="${TOTP_CODE}"
+npm run github-release
