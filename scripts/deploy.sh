18db3f9890bec83a8bc5b78cdcec5593f1a69375
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-01-29 14:07:33 +0000

    fixes: support CI for automation

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,5 +8,11 @@ TOTP_CODE=$(curl -s -X GET "${VAULT_URL}" -H "X-Vault-Token: ${VAULT_NPM_TOTP_TO
 
 ## This is possible as TOTP code is used once and only once
 set -xe
-lerna publish --otp="${TOTP_CODE}"
+
+## Running in the CI therefore it's not required to prompt for the versions
+FLAG=''
+if [ -n "${JENKINS_URL}" ] ||  [ -n "${TRAVIS}" ] ; then
+    FLAG='--yes'
+fi
+lerna publish --otp="${TOTP_CODE}" ${FLAG}
 npm run github-release
